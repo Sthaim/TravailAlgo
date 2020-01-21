4bd6142faeda452a3708204fe93d75a815a6a0f5
DingerM GD1-2 <32112541+Sthaim@users.noreply.github.com>
2020-01-21 21:25:20 +0100

    FinalExo5

--- lol.c
+++ lol.c
@@ -5,8 +5,10 @@ int main()
 {
 	int PVMonstre=100;
 	int PVJoueur=100;
-	int Def=0;
+	int Def=1;
+	int DefMonst=1;
 	int Choix=0;
+	printf("%d\n",Choix);
   printf("Point de vie du monstre :%d\n",PVMonstre);
 	printf("Point de vie du joueur :%d\n",PVJoueur);
 	do{
@@ -19,18 +21,26 @@ int main()
 		if(Choix==1){
 			Def=4;
 		}
-		else if(Choix==2){
+		else {
 			printf("Attaque puissante du joueur\n");
 			printf("-20PV\n");
-			PVMonstre-=20;
+			PVMonstre-=20/DefMonst;
 			printf("Point de vie du monstre :%d\n",PVMonstre);
 			Def=1;
 		}
 		if(PVMonstre>0){
-			printf("Attaque puissante du monstre\n");
-			printf("-20PV\n");
-			PVJoueur-=20/Def;
-			printf("Point de vie du joueur :%d\n",PVJoueur);
+			Choix=rand()%2+1;
+			if(Choix==1){
+				DefMonst=4;
+			}
+			else {
+				printf("Attaque puissante du monstre\n");
+				printf("-20PV\n");
+				PVJoueur-=20/Def;
+				printf("Point de vie du joueur :%d\n",PVJoueur);
+				DefMonst=1;
+			}
+
 		}
 	}
 	while(PVMonstre>0&&PVJoueur>0);
