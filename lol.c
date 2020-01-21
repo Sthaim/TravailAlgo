41ae0f4fc0407c4274509b5ab583d30965b250e0
DingerM GD1-2 <32112541+Sthaim@users.noreply.github.com>
2020-01-21 21:58:41 +0100

    Avec l'Exo7 en bonus

--- lol.c
+++ lol.c
@@ -8,39 +8,69 @@ int main()
 	int Def=1;
 	int DefMonst=1;
 	int Choix=0;
+	int MPJ=10;
+	int MPM=10;
+	int PoisJ=0;
+	int PoisM=0;
 	printf("%d\n",Choix);
   printf("Point de vie du monstre :%d\n",PVMonstre);
 	printf("Point de vie du joueur :%d\n",PVJoueur);
 	do{
-		printf("Choisir entre defense(1) et attaque(2)\n");
+		printf("Choisir entre defense(1) et attaque(2) et Sort de poison(3)\n");
 		scanf("%d",&Choix);
-		while(Choix!=2 && Choix!=1){
-			printf("Choisir entre defense(1) et attaque(2)\n");
+		while(Choix!=2 && Choix!=1 && Choix!=3){
+			printf("Choisir entre defense(1)/attaque(2) et Sort de poison(3)\n");
 			scanf("%d",&Choix);
 		}
+		while(MPJ<=0 && Choix==3){
+			printf("Pas assez de mana\n");
+			printf("Choisir entre defense(1) et attaque(2)\n");
+			scanf("%d",&Choix);
+			}
 		if(Choix==1){
 			Def=4;
 		}
-		else {
+		else if(Choix==2){
 			printf("Attaque puissante du joueur\n");
 			printf("-20PV\n");
 			PVMonstre-=20/DefMonst;
 			printf("Point de vie du monstre :%d\n",PVMonstre);
 			Def=1;
 		}
+		else {
+			MPJ-=2;
+			PoisM=1;
+
+		}
 		if(PVMonstre>0){
-			Choix=rand()%2+1;
+			Choix=rand()%3+1;
+			while(MPM<=0 && Choix==3){
+				Choix=rand()%3+1;
+			}
 			if(Choix==1){
 				DefMonst=4;
 			}
-			else {
+			else if(Choix==2){
 				printf("Attaque puissante du monstre\n");
 				printf("-20PV\n");
 				PVJoueur-=20/Def;
 				printf("Point de vie du joueur :%d\n",PVJoueur);
 				DefMonst=1;
 			}
-
+			else{
+				PoisJ=1;
+				MPM-=2;
+			}
+		}
+		if(PoisM==1){
+			printf("Le poison inflige -5 au monstre\n");
+			PVMonstre-=5;
+			printf("Point de vie du monstre :%d\n",PVMonstre);
+		}
+		if(PoisJ==1){
+			printf("Le poison inflige -5 au joueur\n");
+			PVJoueur-=5;
+			printf("Point de vie du joueur :%d\n",PVJoueur);
 		}
 	}
 	while(PVMonstre>0&&PVJoueur>0);
