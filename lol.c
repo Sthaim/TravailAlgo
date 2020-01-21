d136a7fcab7e7238a9aaf75401551f1d9a59c128
DingerM GD1-2 <32112541+Sthaim@users.noreply.github.com>
2020-01-21 19:07:33 +0100

    Update lol.c

--- lol.c
+++ lol.c
@@ -3,11 +3,14 @@
 
 int main()
 {
-	int PV=100; 
-    printf("Point de vie de %d\n",PV);
+	int PV=100;
+    printf("Point de vie du monstre :%d\n",PV);
+	while(PV>0){
 	printf("Attaque puissante\n");
 	printf("-20PV\n");
 	PV-=20;
+	printf("Point de vie de %d\n",PV);
+	}
 	printf("Point de vie de %d\n",PV);
 	
     return 0;
